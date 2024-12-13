b46e11205117508e8bcf944e7e3ac03e26aeb068
Tim Hanlon <timhanlon01@gmail.com>
2024-12-13 13:37:02 -0500

    third commit

--- project_one.py
+++ project_one.py
@@ -26,3 +26,30 @@ plt.show()
 
 #Pitch Mix for Shota Imanaga
 # Data source: FanGraphs Baseball (https://www.fangraphs.com/)
+pitch_data = {
+    'Pitch Type': ['Four-Seam Fastball', 'Split-Finger', 'Sweeper', 'Changeup', 'Curveball'],
+    'Pitches Thrown': [1347, 792, 199, 101, 84],
+    'Percentage': [52.0, 30.6, 7.7, 3.9, 3.2]
+}
+df = pd.DataFrame(pitch_data)
+
+#Pie chart of pitch mix
+plt.figure(figsize=(10, 7))
+plt.pie(df['Percentage'], labels=df['Pitch Type'], autopct='%1.1f%%')
+plt.title("Shota Imanaga's 2024 Pitch Mix")
+plt.show()
+
+# Additional Pitch Characteristics
+print("Pitch Velocities:")
+print("Four-Seam Fastball: 92 mph")
+print("Split-Finger: 83 mph")
+print("Sweeper: 82 mph")
+print("Changeup: 82 mph")
+print("Curveball: 73 mph")
+
+# Ohtani's performance against fastballs
+fastball_data = {
+    'Velocity': ['95+ mph'],
+    'AVG': [0.360],
+    
+}
